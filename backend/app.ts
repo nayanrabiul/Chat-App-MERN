51992aa6178f2e23dde6f2e5e08a615dfc267a2a
nayanrabiul <nayan.rabiul@gmail.com>
2023-07-06 17:37:45 +0600

    saving

--- backend/app.ts
+++ backend/app.ts
@@ -4,27 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const server = require('http').createServer(app);
+const server = require('http').Server(app)
 
 const io = socket(server, {
     cors: {
         origin: '*',
     }
 });
-io.on('connection', (socket) => {
-    console.log(socket.id);
 
-    socket.on('message', (message) => {
-        console.log('Received message:', message);
-        //emit the message to who is sending the message
-        socket.emit('message', message);
-        // io.emit('message', message); // Broadcast the message to all connected clients
-    });
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
-});
+app.use(chatApp(io))
 
 let Port = process.env.PORT || 6000;
 
